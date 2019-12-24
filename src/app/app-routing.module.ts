29746325567f4735a848459232b073c996a236f3
lautaromsc <lautaromsc@gmail.com>
2019-12-24 12:23:01 -0300

    borrado de modulos y maquetado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,18 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { BandejaComponent } from './bandeja/bandeja.component';
 import { HomeComponent } from './home/home.component';
 import { MainNavComponent } from './main-nav/main-nav.component';
-import { NavbarComponent } from './navbar/navbar.component';
+
 
 const routes: Routes = [
-  
-  { path: '', component: MainNavComponent },
+  { path: '',                 component: MainNavComponent },
   { path: 'MainNavComponent', component: MainNavComponent },
-  
-  { path: 'pos',              component: NavbarComponent },
   { path: 'home',             component: HomeComponent },
-  { path: 'pos',              component: NavbarComponent },
   { path: 'ventas',           component: BandejaComponent },
-
+  
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { NavbarComponent } from './navbar/navbar.component';
 
 
-import { MatToolbarModule, MatButtonModule } from '@angular/material/';
+
+
 import { BandejaComponent } from './bandeja/bandeja.component';
 import { HomeComponent } from './home/home.component';
 import { SidenavComponent } from './sidenav/sidenav.component';
 import { MainNavComponent } from './main-nav/main-nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
+
+import { MatToolbarModule, MatButtonModule } from '@angular/material/';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
@@ -23,7 +25,6 @@ import { MatListModule } from '@angular/material/list';
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
     BandejaComponent,
     HomeComponent,
     SidenavComponent,
