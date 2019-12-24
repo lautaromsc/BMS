7c4af434f88a91e339b23bc6c8199866e3e56547
lautaromsc <lautaromsc@gmail.com>
2019-12-24 02:12:48 -0300

    maquetado de navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BandejaComponent } from './bandeja/bandeja.component';
+import { HomeComponent } from './home/home.component';
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainNavComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'bandeja', component: BandejaComponent },
+
+  { path: 'MainNavComponent', component: MainNavComponent },
+  { path: 'bandeja', component: BandejaComponent },
+
+  { path: 'navBar', component: NavbarComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './navbar/navbar.component';
+
+
+import { MatToolbarModule, MatButtonModule } from '@angular/material/';
+import { BandejaComponent } from './bandeja/bandeja.component';
+import { HomeComponent } from './home/home.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    BandejaComponent,
+    HomeComponent,
+    SidenavComponent,
+    MainNavComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     AppRoutingModule,
     BrowserModule,
-    NgbModule
+    NgbModule,
+
+    MatToolbarModule,
+    MatButtonModule,
+    LayoutModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
