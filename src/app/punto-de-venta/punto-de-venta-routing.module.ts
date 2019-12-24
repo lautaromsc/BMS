66d0dc94228332b79ea506a0ee4eb7efa88a87d1
lautaromsc <lautaromsc@gmail.com>
2019-12-24 13:19:42 -0300

    maquetado de ventas

--- src/app/punto-de-venta/punto-de-venta-routing.module.ts
+++ src/app/punto-de-venta/punto-de-venta-routing.module.ts
@@ -5,8 +5,8 @@ import { PuntoDeVentaComponent } from './punto-de-venta.component';
 import { VentasComponent } from './ventas/ventas.component'
 
 const routes: Routes = [
-  { path: '', component: PuntoDeVentaComponent },
 
+  { path: '', component: PuntoDeVentaComponent },
   { path: 'ventas', component: VentasComponent }
 
 ];
