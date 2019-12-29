7a502e608fa454d5e5158508903245a2242f222d
lautaromsc <lautaromsc@gmail.com>
2019-12-29 01:50:07 -0300

    estructurado de modulos

---
+++ src/app/modules/punto-de-venta/ventas/ventas.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { startWith, map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+
+
+@Component({
+  selector: 'app-ventas',
+  templateUrl: './ventas.component.html',
+  styleUrls: ['./ventas.component.css']
+})
+
+
+export class VentasComponent implements OnInit {
+
+  ngOnInit(){
+    
+  }
+
+}
