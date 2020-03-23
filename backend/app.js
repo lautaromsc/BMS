df15ff22104a6def14e6dacd106fd37d89faf1ef
lautaromsc <lautaromsc@gmail.com>
2020-03-23 02:42:11 -0400

    error handling p1

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan')
 
 const productRoutes = require('./api/routes/products');
 const productOrders = require('./api/routes/orders');
@@ -9,15 +10,14 @@ app.use(morgan('dev'));
 app.use('/products', productRoutes);
 app.use('/orders', productOrders);
 
-
 app.use((req, res, next) => {
     const error = new Error('Not found');
-    error.status(404);
+    error.status = 404;
     next(error);
-});
+})
 
 app.use((error, req, res, next) => {
-    res.status(err.status || 500);
+    res.status(error.status || 500);
     res.json({
         error: {
             message: error.message
